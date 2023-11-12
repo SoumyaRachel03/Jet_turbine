009ab056243fdeabb7f5ee71ef6cabb08c2aad96
Soumya Rachel <soumyasocials@gmail.com>
2023-11-12 11:51:37 +0530

    First Commit

---
+++ Assets/Scripts/QuitMethod.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitMethod : MonoBehaviour
+{
+   public void quit(){
+    Application.Quit();
+   }
+}
